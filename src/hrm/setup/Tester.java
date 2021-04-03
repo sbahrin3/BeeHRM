45927f0bb747aa641d79bd827e61d78a53ed612f
Shamsul Bahrin bin Abd Mutalib <sbahrin3@gmail.com>
2021-04-03 08:32:38 +0800

    Updates

--- src/hrm/setup/Tester.java
+++ src/hrm/setup/Tester.java
@@ -6,7 +6,7 @@ public class Tester {
 	
 	public static void main(String[] args) {
 		
-		
+		// this will let hibernate creates all tables declared in hibernate.cfg.xml
 		Persistence db = Persistence.db();
 		
 	}
