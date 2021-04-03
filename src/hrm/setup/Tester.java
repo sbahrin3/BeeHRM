56b774fc0c01eec3b2564512e32d3b8cf90245e5
Shamsul Bahrin bin Abd Mutalib <sbahrin3@gmail.com>
2021-04-03 08:35:00 +0800

    Test.

--- src/hrm/setup/Tester.java
+++ src/hrm/setup/Tester.java
@@ -1,5 +1,9 @@
 package hrm.setup;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import hrm.entity.Timedemo;
 import lebah.db.entity.Persistence;
 
 public class Tester {
@@ -8,6 +12,12 @@ public class Tester {
 		
 		// this will let hibernate creates all tables declared in hibernate.cfg.xml
 		Persistence db = Persistence.db();
+		
+		List<Timedemo> ts = Persistence.db().list("select t from Timedemo t");
+		Timedemo t = ts.get(0);
+		System.out.println(new SimpleDateFormat("dd/MM/yyyy").format(t.getDate()));
+		System.out.println(new SimpleDateFormat("hh:mm a").format(t.getTimeIn()));
+		System.out.println(new SimpleDateFormat("hh:mm a").format(t.getTimeOut()));
 		
 	}
 
